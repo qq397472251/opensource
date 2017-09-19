2a40527a86119fae6ef899ba9647153faaebe62b
zxy219319@sina.com <zxy219319@sina.com>
2017-09-19 16:28:47 +0800

    zxy create

---
+++ consoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace consoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" easyStudy2017-9-19 16:24:34");
+            Console.Read();
+        }
+    }
+}
